4ac11aeea7ffa85e1424c5defc4c03bdfaa5408a
Mark W <numedain@gmail.com>
2026-01-28 15:06:03 +0100

    Minor code cleanup

--- core.lua
+++ core.lua
@@ -7,7 +7,7 @@ _G.Numdelicious_QoL_Tweaks = ns;
 if not _G.NumQT then _G.NumQT = ns; end
 --@end-debug@
 
---- @class NQT_Main: AceAddon, AceConsole-3.0, AceHook-3.0, AceEvent-3.0
+--- @class NQT_Main: NumyConfig_AceAddon, AceConsole-3.0, AceHook-3.0, AceEvent-3.0
 local Main = LibStub('AceAddon-3.0'):NewAddon(name, 'AceConsole-3.0', 'AceHook-3.0', 'AceEvent-3.0');
 if not Main then return; end
 ns.Main = Main;
